a99d787cae32885a065fad4eb3d46e0765560852
Luukas Mepham <luukas.mepham@icloud.com>
2023-11-24 19:15:12 +0200

    Added some cucumber scenarios

--- cucumber/features/steps/steps.py
+++ cucumber/features/steps/steps.py
@@ -1,13 +1,76 @@
 from behave import *
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
-@given('url is for DuckDuckGo')
-def step(context):
-	context.driver.get("https://www.duckduckgo.com/")
-	print("Opened DuckDuckGo")
+@given('open webpage "{url}"')
+def open_url(context, url):
+	context.driver.get(url)
+	context.driver.maximize_window()
 	
-@then('verify url is for DuckDuckGo — Privacy, simplified.')
-def step(context):
-	if context.driver.title == "DuckDuckGo — Privacy, simplified.":
-		print("Verified title is DuckDuckGo — Privacy, simplified.")
-	else:
-		print("Verified title is not DuckDuckGo — Privacy, simplified.")
+@step('click "{element}"')
+def click_element(context, element):
+	match element:
+		case "Sign in":
+			context.driver.implicitly_wait(10)
+			context.driver.find_element(By.XPATH, '/html/body/div[2]/header/div[1]/div/ul/li[2]/a').click()
+		case "Sign out":
+			context.driver.implicitly_wait(10)
+			context.driver.find_element(By.XPATH, '//button[@data-action="customer-menu-toggle"]').click()
+			context.driver.find_element(By.XPATH, '//a[@href="https://magento.softwaretestingboard.com/customer/account/logout/"]').click()
+		case "Create an Account":
+			context.driver.implicitly_wait(10)
+			context.driver.find_element(By.XPATH, '//a[text()="Create an Account"]').click()
+		case "Submit account creation form":
+			context.driver.find_element(By.CSS_SELECTOR, '.submit').click()
+		case "Submit account sign in form":
+			context.driver.find_element(By.CSS_SELECTOR, '#send2').click()
+		case "Sale":
+			context.driver.find_element(By.CSS_SELECTOR, '#ui-id-8').click()
+		case "Jackets":
+			context.driver.find_element(By.XPATH, '//a[text()="Jackets"]').click()
+	
+@step('verify "{condition}"')
+def verify(context, condition):
+	match condition:
+		case "Account sign in page":
+			context.driver.implicitly_wait(5)
+			context.driver.find_element(By.CSS_SELECTOR, '#email')
+			assert context.driver.title == "Customer Login"
+		case "Account creation page":
+			context.driver.implicitly_wait(5)
+			context.driver.find_element(By.CSS_SELECTOR, '#firstname')
+			assert context.driver.title == "Create New Customer Account"
+		case "Account created succesfully":
+			context.driver.implicitly_wait(5)
+			context.driver.find_element(By.XPATH, '//div[text()="Thank you for registering with Main Website Store."]')
+		case "Account signed in succesfully":
+			context.driver.implicitly_wait(5)
+			context.driver.find_element(By.CSS_SELECTOR, '.logged-in')
+		case "Account signed out succesfully":
+			context.driver.implicitly_wait(3)
+			context.driver.find_element(By.CSS_SELECTOR, '.customer-account-logoutsuccess')
+		case "Sale page":
+			context.driver.implicitly_wait(5)
+			context.driver.find_element(By.XPATH, '//span[text()="Sale"]')
+			assert context.driver.title == "Sale"
+		case "Jackets page":
+			context.driver.implicitly_wait(5)
+			context.driver.find_element(By.XPATH, '//span[text()="Jackets"]')
+			assert context.driver.title == "Jackets - Tops - Women"
+			
+@step('fill form field "{field}" "{value}"')
+def fill_form(context, field, value):
+	match field:
+		case "Create first name":
+			context.driver.find_element(By.CSS_SELECTOR, '#firstname').send_keys(value)
+		case "Create last name":
+			context.driver.find_element(By.CSS_SELECTOR, '#lastname').send_keys(value)
+		case "Create email":
+			context.driver.find_element(By.CSS_SELECTOR, '#email_address').send_keys(value)
+		case "Create password":
+			context.driver.find_element(By.CSS_SELECTOR, '#password').send_keys(value)
+			context.driver.find_element(By.CSS_SELECTOR, '#password-confirmation').send_keys(value)
+		case "Password":
+			context.driver.find_element(By.CSS_SELECTOR, '#pass').send_keys(value)
+		case "Email":
+			context.driver.find_element(By.CSS_SELECTOR, '#email').send_keys(value)
--- cucumber/features/test.feature
+++ cucumber/features/test.feature
@@ -1,5 +1,44 @@
-Feature: Experimenting
+Feature: test scenarios
 
-Scenario: Visit DuckDuckGo
-  Given url is for DuckDuckGo
-  Then verify url is for DuckDuckGo — Privacy, simplified.
+### ONE TIME USE ONLY! ###
+@accountCreation
+Scenario: create an account
+	Given open webpage "https://magento.softwaretestingboard.com/"
+	When click "Create an Account"
+	Then verify "Account creation page"
+	When fill form field "Create first name" "Test"
+	When fill form field "Create last name" "User"
+	When fill form field "Create email" "testuser@teskjhkjht.test"
+	When fill form field "Create password" "Password123"
+	When click "Submit account creation form"
+	Then verify "Account created succesfully"
+  
+@signin
+Scenario: sign user in and sign user out
+	Given open webpage "https://magento.softwaretestingboard.com/"
+	When click "Sign in"
+	Then verify "Account sign in page"
+	When fill form field "Email" "testuser@teskjhkjht.test"
+	When fill form field "Password" "Password123"
+	When click "Submit account sign in form"
+	Then verify "Account signed in succesfully"
+	When click "Sign out"
+	Then verify "Account signed out succesfully"
+	
+@purchase
+Scenario: purchase a product
+	Given open webpage "https://magento.softwaretestingboard.com/"
+	
+	# SIGN IN
+	When click "Sign in"
+	Then verify "Account sign in page"
+	When fill form field "Email" "testuser@teskjhkjht.test"
+	When fill form field "Password" "Password123"
+	When click "Submit account sign in form"
+	Then verify "Account signed in succesfully"
+	
+	# NAVIGATE TO SALES PAGE
+	When click "Sale"
+	When click "Jackets"
+
+  
